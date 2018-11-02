7ab53c14c21b45e15dfbaaad696094777d24bc76
dgole <dgole100@gmail.com>
2018-11-02 14:08:44 -0600

    now read phst

--- athenaReaderPhst.py
+++ athenaReaderPhst.py
@@ -5,61 +5,55 @@ import matplotlib.pyplot as plt
 import os
 import matplotlib.colors as colors
 
-#m.rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
-
-def getTimeStepString(i):
-	if i > 999:	 zstring = ""
-	elif i > 99: zstring = "0"
-	elif i > 9:  zstring = "00"
-	elif i > -1: zstring = "000"
-	return zstring+str(i)
-
 ####################################################################
 # Data class ###############################################
 ####################################################################
-class Data1d:
+class DataPhst:
 	def __init__(self, path, baseName="Par_Strat3d", dt=0.1):
 		print("initializing 1d data structure from " + path)
-		self.path    = path
-		fileList     = os.listdir(self.path)
-		nFiles       = len(fileList)
-		names        = [baseName + "." + getTimeStepString(n) + ".1d" for n in range(nFiles)]
-		files        = [np.loadtxt(path+names[n]) for n in range(nFiles)]
-		dataArray    = np.asarray(files)
-		self.data    = {'rho'     : dataArray[:,:,1],
-						'P'       : dataArray[:,:,2],
-						'KEx'     : dataArray[:,:,3],
-						'KEy'     : dataArray[:,:,4],
-						'KEz'     : dataArray[:,:,5],
-						'KE'      : dataArray[:,:,6],
-						'reynolds': dataArray[:,:,7]}
-		self.header  = {'rho'     : r"$\rho$",
-						'P'       : r"$P$",
-						'KEx'     : r"$KE_x$",
-						'KEy'     : r"$KE_y$",
-						'KEz'     : r"$KE_z$",
-						'KE'      : r"$KE$",
-						'reynolds': r"$\rho v_x \delta v_y$"}
-		self.z      = dataArray[0,:,0]
-		self.nt     = self.data['rho'].shape[0]
-		self.nz     = self.data['rho'].shape[1]
-		self.zmax   = np.round(-self.z[0],1)
-		self.tmax   = dt*self.nt
-		self.t      = np.arange(0, self.tmax, dt)
-		del files, dataArray
-		print("data of shape " + str(self.data['rho'].shape) + " imported")
-	def getzindex(self, z):
-		return (np.abs(self.z-z)).argmin()
+		self.path = path
+		fileName  = baseName + '.phst'
+		inFile    = open(path+fileName, 'r')
+		list1 = []; list2 = []
+		for line in inFile:
+			if line[0] != '#':
+				split = line.split()
+				arr   = np.zeros(len(split))
+				n = 0
+				for s in split:	arr[n] = float(s); n+=1;
+				if len(arr) == 11: list1.append(arr)
+				if len(arr) == 12: list2.append(arr)
+		data1 = np.asarray(list1)
+		data2 = np.asarray(list2)
+		self.data    = {'t'      : data1[:,0],
+						'xavg'   : data2[:,0],
+						'yavg'   : data2[:,1],
+						'zavg'   : data2[:,2],
+						'vxavg'  : data2[:,3],
+						'vyavg'  : data2[:,4],
+						'vzavg'  : data2[:,5],
+						'xvar'   : data2[:,6],
+						'yvar'   : data2[:,7],
+						'zvar'   : data2[:,8],
+						'vxvar'  : data2[:,9],
+						'vyvar'  : data2[:,10],
+						'vzvar'  : data2[:,11]}
+		del list1, list2, arr, split, data1, data2
+		#self.header  = {'rho'     : r"$\rho$",
+		#				'reynolds': r"$\rho v_x \delta v_y$"}
+		self.t      = self.data['t']
+		print("phst data of length " + str(self.data['xavg'].shape) + " imported")
 	def gettindex(self, t):
 		return (np.abs(self.t-t)).argmin()
-	def addCol(self, funcName, key, headerLabel, *args, **kwargs):
-		print(self.path + ": adding data with key " + key)
-		self.data[key]   = funcName(self)
-		self.header[key] = headerLabel
+	#def addCol(self, funcName, key, headerLabel, *args, **kwargs):
+	#	print(self.path + ": adding data with key " + key)
+	#	self.data[key]   = funcName(self)
+	#	self.header[key] = headerLabel
 
 ####################################################################
 # plotting functions ###############################################
 ####################################################################
+'''
 def stPlot(do, key, figNum=0):
 	print (do.path + ": making ST plot for key " + key)
 	plt.figure(figNum)
@@ -80,7 +74,8 @@ def stPlot(do, key, figNum=0):
 	plt.ylabel(r"$z/H$");
 	plt.colorbar(shrink=0.5)
 	plt.tight_layout()
-
+'''
+'''
 def profile(do, key, figNum=0, tStart=None, tEnd=None, legendLabel=None):
 	print(do.path + ": making profile plot for key " + key)
 	plt.figure(figNum)
@@ -94,18 +89,20 @@ def profile(do, key, figNum=0, tStart=None, tEnd=None, legendLabel=None):
 	plt.ylabel(do.header[key]);
 	plt.xlabel(r"$z/H$");
 	plt.tight_layout()
+'''
 
 def timeEvo(do, key, figNum=0, legendLabel=None):
 	print(do.path + ": making timeEvo plot for key " + key)
 	plt.figure(figNum)
-	plotData = np.mean(do.data[key], axis=1)
+	plotData = do.data[key]
 	if 10.0*np.amin(np.absolute(plotData[2:])) < np.amax(np.absolute(plotData[2:])):
 		plt.semilogy(do.t, np.absolute(plotData), label=legendLabel)
 	else:
 		plt.plot(do.t, np.absolute(plotData), label=legendLabel)
-	plt.ylabel(do.header[key]);
+	plt.ylabel(key);
 	plt.xlabel(r"$t \Omega$");
 	plt.tight_layout()
-
+'''
 def dv(do):
 	return np.sqrt(2.0*do.data['KE']/do.data['rho'])
+'''
--- workspace.py
+++ workspace.py
@@ -6,45 +6,46 @@ import os
 import math
 import sys
 import athenaReader1d as reader1d
+import athenaReaderPhst as readerPhst
 from matplotlib.backends.backend_pdf import PdfPages
 
+doGas1d  = 0
+doParAvg = 1
 
-path     = '../data/turbTest/run1/'
-dataPath = path + '1d/'
-savePath  = path + 'plots/'
+# data path and output path
+path        = '../data/' + str(sys.argv[1])
+savePath    = path + 'plots/'
 if not os.path.exists(savePath): os.makedirs(savePath)
-do1d    = reader1d.Data(dataPath)
 
-for col in range(1,7):
-	reader1d.stPlot(do1d, col)
-	plt.savefig(savePath + "ST_" + str(col) + ".png", bbox_inches='tight')
-	plt.clf()
+# 1d GAS
+if doGas1d == 1:
+	dataPath1d  = path + '1d/'
+	do1d = reader1d.Data1d(dataPath1d)
+	do1d.addCol(reader1d.dv, 'dv', r'$\delta v$')
 
-col = 7
-reader1d.stPlot(do1d, col, cmapType='coolwarm')
-plt.savefig(savePath + "ST_" + str(col) + ".png", bbox_inches='tight')
-plt.clf()
+	for key in do1d.data.keys():
+		reader1d.stPlot(do1d, key)
+		plt.savefig(savePath + "gas_ST_" + key + ".png", bbox_inches='tight')
+		plt.clf()
 
+	for key in do1d.data.keys():
+		reader1d.profile(do1d, key)
+		plt.savefig(savePath + "gas_profile_" + key + ".png", bbox_inches='tight')
+		plt.clf()
 
-
-
-
-
-
-
-
-
-
-
+	for key in do1d.data.keys():
+		reader1d.timeEvo(do1d, key)
+		plt.savefig(savePath + "gas_timeEvo_" + key + ".png", bbox_inches='tight')
+		plt.clf()
 
 
-
-
-
-
-
-
-
+# PARTICLE AVERAGES
+if doParAvg == 1:
+	doPhst = readerPhst.DataPhst(path)
+	for key in ['zvar', 'vzvar']:
+		readerPhst.timeEvo(doPhst, key)
+		plt.savefig(savePath + "par_timeEvo_" + key + ".png", bbox_inches='tight')
+		plt.clf()
 
 
 
@@ -57,23 +58,14 @@ plt.clf()
 
 
 
-# add column and plot it
-#run1d[0].addCol(reader.dv1d, r"$ \delta v / c_s $")
-#run1d[0].profile(19, t1, t2, logOption=1, save="pdf")
-#run1d[0].stPlot(19, logOption=1, save="pdf")
 
-#run1d[0].addCol(reader.transEff, "Transport Effeciency")
-#run1d[0].profile(20, t1, t2, logOption=1, save="pdf")
-#run1d[0].stPlot(20, logOption=1, clim1=-2, save="pdf")
 
-# multiple timeEvos plot
-#col=18
-#run1d[0].timeEvo(col, -3.0, -2.0, logOption=1, legendLabel="-z")
-#run1d[0].timeEvo(col, -0.5,  0.5, logOption=1, legendLabel="midplane")
-#run1d[0].timeEvo(col,  2.0,  3.0, logOption=1, legendLabel="+z")
-#plt.savefig(run1d[0].pdfName, format='pdf'); plt.clf();
 
-# multiple profiles plot
-#run1d[0].profile(18, t1, t2, logOption=1, legendLabel="maxwell")
-#run1d[0].profile(10, t1, t2, logOption=1, legendLabel="reynolds")
-#plt.savefig(run1d[0].pdfName, format='pdf'); plt.clf();
+
+
+
+
+
+
+
+#
