586d7c1a9f09fa07afa3a69eee3007e56ceb2e6c
dgole <dgole100@gmail.com>
2018-11-02 15:08:22 -0600

    made workspace a little smoother

--- workspace.py
+++ workspace.py
@@ -9,7 +9,7 @@ import athenaReader1d as reader1d
 import athenaReaderPhst as readerPhst
 from matplotlib.backends.backend_pdf import PdfPages
 
-doGas1d  = 0
+doGas1d  = 1
 doParAvg = 1
 
 # data path and output path
@@ -17,35 +17,30 @@ path        = '../data/' + str(sys.argv[1])
 savePath    = path + 'plots/'
 if not os.path.exists(savePath): os.makedirs(savePath)
 
+def saveAndClear(plotName, figNum=0):
+	plt.figure(figNum)
+	plt.savefig(savePath + plotName, bbox_inches='tight')
+	plt.clf()
+
 # 1d GAS
 if doGas1d == 1:
 	dataPath1d  = path + '1d/'
 	do1d = reader1d.Data1d(dataPath1d)
 	do1d.addCol(reader1d.dv, 'dv', r'$\delta v$')
-
 	for key in do1d.data.keys():
 		reader1d.stPlot(do1d, key)
-		plt.savefig(savePath + "gas_ST_" + key + ".png", bbox_inches='tight')
-		plt.clf()
-
-	for key in do1d.data.keys():
+		saveAndClear("gas_ST_" + key + ".png")
 		reader1d.profile(do1d, key)
-		plt.savefig(savePath + "gas_profile_" + key + ".png", bbox_inches='tight')
-		plt.clf()
-
-	for key in do1d.data.keys():
+		saveAndClear("gas_profile_" + key + ".png")
 		reader1d.timeEvo(do1d, key)
-		plt.savefig(savePath + "gas_timeEvo_" + key + ".png", bbox_inches='tight')
-		plt.clf()
-
+		saveAndClear("gas_timeEvo_" + key + ".png")
 
 # PARTICLE AVERAGES
 if doParAvg == 1:
 	doPhst = readerPhst.DataPhst(path)
 	for key in ['zvar', 'vzvar']:
 		readerPhst.timeEvo(doPhst, key)
-		plt.savefig(savePath + "par_timeEvo_" + key + ".png", bbox_inches='tight')
-		plt.clf()
+		saveAndClear("par_timeEvo_" + key + ".png")
 
 
 
