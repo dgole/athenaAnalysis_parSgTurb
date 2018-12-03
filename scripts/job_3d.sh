b7f5b21f93d1284e6cf69d61f01142a92f00cfb0
Daniel Gole <tg833898@login3.stampede2.tacc.utexas.edu>
2018-12-03 13:05:22 -0600

    changed 3d job script

--- scripts/job_3d.sh
+++ scripts/job_3d.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH -J 3d120
+#SBATCH -J 3d
 ##SBATCH --time=48:00:00
 #SBATCH --time=1:00:00
 #SBATCH -N 1
@@ -11,29 +11,54 @@
 # The following commands will be executed when this script is run.
 module load gcc python3
 
-export DIR=../../data/turbTest/run30/bin/
-python3 quickLook.py $DIR
+#export DIR=../../data/turbTest/run30/bin/
+#python3 quickLook.py $DIR
+#python3 plots3d.py   $DIR
+#python3 pspec.py     $DIR
+#python3 acf.py       $DIR
+
+#export DIR=../../data/turbTest/run31/bin/
+#python3 quickLook.py $DIR
+#python3 plots3d.py   $DIR
+#python3 pspec.py     $DIR
+#python3 acf.py       $DIR
+
+#export DIR=../../data/turbTest/run32/bin/
+#python3 quickLook.py $DIR
+#python3 plots3d.py   $DIR
+#python3 pspec.py     $DIR
+#python3 acf.py       $DIR
+
+#export DIR=../../data/turbTest/run33/bin/
+#python3 quickLook.py $DIR
+#python3 plots3d.py   $DIR
+#python3 pspec.py     $DIR
+#python3 acf.py       $DIR
+
+export DIR=../../data/turbTest/run34/bin/
+#python3 quickLook.py $DIR
 python3 plots3d.py   $DIR
 python3 pspec.py     $DIR
 python3 acf.py       $DIR
 
-#export DIR=../../data/turbTest/run30/bin/
+export DIR=../../data/turbTest/run120/bin/
 #python3 quickLook.py $DIR
-#python3 plots3d.py   $DIR
-#python3 pspec.py     $DIR
-#python3 acf.py       $DIR
+python3 plots3d.py   $DIR
+python3 pspec.py     $DIR
+python3 acf.py       $DIR
 
-#export DIR=../../data/turbTest/run30/bin/
+export DIR=../../data/turbTest/run122/bin/
 #python3 quickLook.py $DIR
-#python3 plots3d.py   $DIR
-#python3 pspec.py     $DIR
-#python3 acf.py       $DIR
+python3 plots3d.py   $DIR
+python3 pspec.py     $DIR
+python3 acf.py       $DIR
 
-#export DIR=../../data/turbTest/run30/bin/
+export DIR=../../data/turbTest/run124/bin/
 #python3 quickLook.py $DIR
-#python3 plots3d.py   $DIR
-#python3 pspec.py     $DIR
-#python3 acf.py       $DIR
+python3 plots3d.py   $DIR
+python3 pspec.py     $DIR
+python3 acf.py       $DIR
+
 
 
 
