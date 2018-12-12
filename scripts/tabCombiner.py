bf312198a1216848e90ed6987ac97654bc9c0884
dgole <dgole100@gmail.com>
2018-12-12 14:35:33 -0700

    tab combiner now not in constant loop

--- scripts/tabCombiner.py
+++ scripts/tabCombiner.py
@@ -21,19 +21,16 @@ def getFiles(basename, path, names):
 	files = []
 	fileNum = 0
 	for name in names:
-		print('#')
-		if myNPC>1: print('process number: ' + mp.current_process().name)
-		print('reading in file number ' + str(fileNum) + ' of ' + str(len(names)))
-		print("total MB of memory used: " + str(float(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)/1000.0))
-		print('#')
-		sys.stdout.flush()
+		#print('#')
+		#if myNPC>1: print('process number: ' + mp.current_process().name)
+		#print('reading in file number ' + str(fileNum) + ' of ' + str(len(names)))
+		#print("total MB of memory used: " + str(float(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)/1000.0))
+		#print('#')
+		#sys.stdout.flush()
 		files.append(np.loadtxt(path+name))
 		fileNum+=1
 	return files
 def combTabs(basename, path, npc, timeStep):
-	global nProcNow
-	global nProcTot
-	global nDoingDict
 	outDir = path+'3d/'
 	# read in files
 	files=getFiles(basename, path, getFileNames(basename, timeStep, npc))
@@ -47,31 +44,29 @@ def combTabs(basename, path, npc, timeStep):
 	masterLength=len(files)*files[0].shape[0];
 	fileNum = 0
 	for file in files:
-		print('#')
-		if myNPC>1: print('process number: ' + mp.current_process().name)
-		print('shuffling file number ' + str(fileNum) + ' of ' + str(len(files)))
-		print("total MB of memory used: " + str(float(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)/1000.0))
-		print('#')
-		sys.stdout.flush()
+		#print('#')
+		#if myNPC>1: print('process number: ' + mp.current_process().name)
+		#print('shuffling file number ' + str(fileNum) + ' of ' + str(len(files)))
+		#print("total MB of memory used: " + str(float(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)/1000.0))
+		#print('#')
+		#sys.stdout.flush()
 		fileNum+=1
 		for i in range(file.shape[0]):
 			indicies=[(np.abs(coordsListArray[j]-file[i,3+j])).argmin() for j in range(3)]
 			masterArray[indicies[0],indicies[1],indicies[2]]=file[i,3:cols]
 	# export table
-	print('#')
-	if myNPC>1: print('process number: ' + mp.current_process().name)
-	print('writing numpy file')
-	print("total MB of memory used: " + str(float(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)/1000.0))
-	print('#')
-	sys.stdout.flush()
+	#print('#')
+	#if myNPC>1: print('process number: ' + mp.current_process().name)
+	#print('writing numpy file')
+	#print("total MB of memory used: " + str(float(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)/1000.0))
+	#print('#')
+	#sys.stdout.flush()
 	np.save(outDir+basename+"."+tools.getTimeStepString(timeStep)+".npy", masterArray)
 	del files
 	del masterArray
 	del coordsList
 	del coordsArray
 	del coordsListArray
-	nProcNow-=1
-	nDoingDict[path]-=1
 def getNpc(path):
 	contentsList = os.listdir(path)
 	npc=0
@@ -97,56 +92,41 @@ basename="Par_Strat3d"
 pathList = []
 for path in sys.argv[2:]:
 	pathList.append(path)
-	nDoingDict[path]=0
 ###############################################################################
-while True:
-	for path in pathList:
-		nDoing = nDoingDict[path]
-		outDir = path+'3d/'
-		if not os.path.exists(outDir): os.makedirs(outDir)
-		npc     = getNpc(path)
-		nAvail = getnAvail(path)
-		nDone  = getnDone(path)
-		print(' ')
-		print('######################################################################')
-		print(str(nProcNow) + ' processes currently running')
-		print('checking ' + path)
-		print(str(nAvail) + ' time steps available')
-		print(str(nDone) + ' time steps already done')
-		print(str(nDoing) + ' time steps currently being done')
-		sys.stdout.flush()
-		if nAvail > nDone + nDoing:
-			timeStep = nDone + nDoing
-			if myNPC>1:
-				while True:
-					if nProcNow < myNPC:
-						print('starting process to do ' + path + ', n=' + str(timeStep))
-						print('######################################################################')
-						print(' ')
-						sys.stdout.flush()
-						p = mp.Process(name=str(nProcTot), target=combTabs, args=(basename, path, npc, timeStep))
-						nProcNow += 1
-						nProcTot += 1
-						p.start()
-						nDoingDict[path] += 1
-						break;
-					else:
-						print('at max number of processes, waiting...')
-						print('######################################################################')
-						print(' ')
-						sys.stdout.flush()
-						time.sleep(10)
-			else:
+for path in pathList:
+	outDir = path+'3d/'
+	if not os.path.exists(outDir): os.makedirs(outDir)
+	npc     = getNpc(path)
+	nAvail = getnAvail(path)
+	nDone  = getnDone(path)
+	print(' ')
+	print('######################################################################')
+	print(str(nProcNow) + ' processes currently running')
+	print('checking ' + path)
+	print(str(nAvail) + ' time steps available')
+	print(str(nDone) + ' time steps already done')
+	sys.stdout.flush()
+	if nAvail > nDone:
+		timeStepList = np.arange(nDone, nAvail+1)
+		if myNPC>1:
+			for timeStep in timeStepList:
+				print('starting process to do ' + path + ', n=' + str(timeStep))
+				sys.stdout.flush()
+				p = mp.Process(name=str(nProcTot), target=combTabs, args=(basename, path, npc, timeStep))
+				nProcTot += 1
+				p.start()
+		else:
+			for timeStep in timeStepList:
 				print('starting ' + path + ', n=' + str(timeStep))
 				sys.stdout.flush()
-				nProcNow +=1
 				combTabs(basename, path, npc, timeStep)
-		else:
-			print('all avaliable output is done or currently being done')
-			print('will check again in 10 seconds...')
-			print('######################################################################')
-			sys.stdout.flush()
-			time.sleep(10)
+	else:
+		print('all avaliable output is done or currently being done')
+		print('will check again in 10 seconds...')
+		print('######################################################################')
+		print(' ')
+		sys.stdout.flush()
+		time.sleep(10)
 
 
 
