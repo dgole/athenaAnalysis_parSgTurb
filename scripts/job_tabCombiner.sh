ac09a68388af26e505d1e312534753984d335b5c
Daniel Gole <tg833898@login4.stampede2.tacc.utexas.edu>
2018-12-11 22:15:05 -0600

    job script change

--- scripts/job_tabCombiner.sh
+++ scripts/job_tabCombiner.sh
@@ -3,7 +3,7 @@
 #SBATCH -J tc
 #SBATCH --time=1:00:00
 #SBATCH -N 1
-#SBATCH --ntasks 1
+#SBATCH --ntasks 64 
 #SBATCH -p development
 
 # The following commands will be executed when this script is run.
