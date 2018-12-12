d451755d44ab395ab369792957d3eecf6d0d9822
Daniel Gole <tg833898@login1.stampede2.tacc.utexas.edu>
2018-12-12 11:35:29 -0600

    small change to tc job

--- scripts/job_tabCombiner.sh
+++ scripts/job_tabCombiner.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #SBATCH -J tc
-#SBATCH --time=1:00:00
+#SBATCH --time=48:00:00
 #SBATCH -N 1
 #SBATCH --ntasks 64 
-#SBATCH -p development
+#SBATCH -p normal
 
 # The following commands will be executed when this script is run.
 
