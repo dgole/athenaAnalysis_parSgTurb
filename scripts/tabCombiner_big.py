04f695ca88893161c0e9776e4dcdf0e7f46316e2
dgole <dgole100@gmail.com>
2019-02-14 18:20:50 -0700

    big tab combiner should be good I think

--- scripts/tabCombiner_big.py
+++ scripts/tabCombiner_big.py
@@ -17,22 +17,21 @@ if not os.path.exists(outDir): os.makedirs(outDir)
 
 ################################################################################
 
-def getFile(path, name, resultList):
-	print('reading in ' + path + name)
+def processFile(path, name, resultList):
+	print('processing ' + path + name)
 	sys.stdout.flush()
 	file = np.loadtxt(path+name)
-	resultList.append(file)
-def assignToArray(file, fileNum, resultList):
-	print('assigning file ' + str(fileNum) + ' of ' + str(npc))
-	sys.stdout.flush()
-	masterArray = np.zeros([coordsListArray[0].shape[0],
-							coordsListArray[1].shape[0],
-							coordsListArray[2].shape[0],
-							cols-3])
-	for i in range(file.shape[0]):
-		indicies=[(np.abs(coordsListArray[j]-file[i,3+j])).argmin() for j in range(3)]
-		masterArray[indicies[0],indicies[1],indicies[2]]=file[i,3:cols]
-	resultList.append(masterArray)
+	coordsList      = file[:,3:6]
+	coordsArray     = np.asarray(coordsList)
+	coordsListArray = [np.unique(coordsArray[:,i]) for i in range(3)]
+	data1 = file.reshape((len(coordsListArray[0]),
+		 				  len(coordsListArray[1]),
+						  len(coordsListArray[2]),
+						  file.shape[1]),
+						  order='F')
+	data = data1[:,:,:,3:]
+	del file, data1
+	resultList.append(data)
 
 ################################################################################
 
@@ -42,69 +41,54 @@ while True:
 	nDone  = tools.getnDone(path)
 	print('checking ' + path)
 	print(str(nAvail) + ' time steps available')
-	print(str(nDone) + ' time steps already done')
+	print(str(nDone)  + ' time steps already done')
 	sys.stdout.flush()
 	if nAvail > nDone:
 		n = nDone
 		########################################################################
 		# read in files (parallel)
-		fileNames = tools.getFileNames(baseName, n, npc)
-		manager   = mp.Manager()
-		files     = manager.list()
+		fileNames   = tools.getFileNames(baseName, n, npc)
+		manager     = mp.Manager()
+		resultsList = manager.list()
 		jobs=[]
 		for name in fileNames:
-			proc=mp.Process(target=getFile, args=(path,name,files))
+			proc=mp.Process(target=processFile, args=(path,name,resultsList))
 			jobs.append(proc)
 		nRead=0
 		while nRead < npc:
-			for n1 in range(myNPC):
-				jobs[nRead+n1].start()
-			for n1 in range(myNPC):
-				jobs[nRead+n1].join()
+			for n1 in range(myNPC): jobs[nRead+n1].start()
+			for n1 in range(myNPC): jobs[nRead+n1].join()
 			nRead+=myNPC
 			tools.printMem('reading in...')
 		tools.printMem('files are read in')
 		########################################################################
-		# set-up (serial)
-		res             = abs(1.0/(files[0][0,3]-files[0][1,3]))
-		cols            = files[0].shape[1]
-		coordsList      = [file[:,3:6] for file in files]
-		coordsArray     = np.asarray(coordsList)
-		coordsListArray = [np.unique(coordsArray[:,:,i]) for i in range(3)]
-		masterArray     = np.zeros([coordsListArray[0].shape[0],
-									coordsListArray[1].shape[0],
-									coordsListArray[2].shape[0],
-									cols-3])
-		masterLength    = len(files)*files[0].shape[0];
-		tools.printMem('master array is set up')
-		########################################################################
-		# assign data to 3d array
-		manager = mp.Manager()
-		resultList = manager.list()
-		jobs=[]; fileNum=0
-		for file in files:
-			proc=mp.Process(target=assignToArray, args=(file,fileNum,resultList))
-			jobs.append(proc)
-			fileNum+=1
-		nRead=0
-		while nRead < npc:
-			resultList = manager.list()
-			for n1 in range(myNPC):
-				jobs[nRead+n1].start()
-			for n1 in range(myNPC):
-				jobs[nRead+n1].join()
-			for result in resultList:
-				masterArray+=result
-			nRead+=myNPC
-			tools.printMem('assigning...')
-		tools.printMem('master array is assigned')
+		# finish up (serial)
+		data   = resultsList[0]
+		nx     = data.shape[0]
+		ny     = data.shape[1]
+		nz     = data.shape[2]
+		cols   = data.shape[3]
+		nTot   = nx*ny*nz*npc
+		n1d    = int(np.round(np.power(nTot, 1./3.)))
+		res    = np.absolute(data[0,0,0,0]-data[1,0,0,0])
+		cLim   = int(res*n1d*100.0); cLim/=200.0;
+		cArray = np.arange(-cLim+res/2.0, cLim, res)
+		masterArray = np.zeros((n1d, n1d, n1d, cols))
+		for data in resultsList:
+			xmin = np.amin(data[:,:,:,0])
+			ymin = np.amin(data[:,:,:,1])
+			zmin = np.amin(data[:,:,:,2])
+			xi   = np.argmin(np.absolute(cArray-xmin))
+			yi   = np.argmin(np.absolute(cArray-ymin))
+			zi   = np.argmin(np.absolute(cArray-zmin))
+			masterArray[xi:xi+nx,yi:yi+ny,zi:zi+nz]=data
 		########################################################################
 		print('writing master file')
 		sys.stdout.flush()
 		np.save(outDir+baseName+"."+tools.getTimeStepString(n)+".npy", masterArray)
 		print('writing is done')
 		sys.stdout.flush()
-		del files, masterArray, coordsList, coordsArray, coordsListArray
+		del masterArray, resultsList
 		time.sleep(5)
 	else:
 		print('all avaliable output is done')
