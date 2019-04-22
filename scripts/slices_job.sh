ef7b8614b791217d3e9104c90d91646c00a083e1
Daniel Gole <tg833898@login1.stampede2.tacc.utexas.edu>
2019-04-22 13:16:55 -0500

    added to gitignore, made slices job

---
+++ scripts/slices_job.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH -J 3d
+#SBATCH --time=2:00:00
+#SBATCH -N 1
+#SBATCH --ntasks 64
+#SBATCH -p development
+
+# The following commands will be executed when this script is run.
+module load gcc
+module load python3
+
+DIR=$SCRATCH/parSgTurb/data/prodRuns/run100/bin/
+
+python3 slices.py $DIR 
+
+
+
+
+
+
+
+#
