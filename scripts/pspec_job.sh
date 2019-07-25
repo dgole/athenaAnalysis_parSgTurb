2d498f2b3a9f49f97ffe3e5c97d12ee214c89073
Daniel Gole <tg833898@login3.stampede2.tacc.utexas.edu>
2019-07-24 22:51:07 -0500

    pspec job from stamp

---
+++ scripts/pspec_job.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH -J 3d_100
+#SBATCH --time=2:00:00
+#SBATCH -N 1
+#SBATCH --tasks-per-node 1
+#SBATCH -p development
+
+# The following commands will be executed when this script is run.
+module load gcc
+module load python3
+
+
+DIR=$SCRATCH/parSgTurb/data/prodRuns/run100/bin/
+python3 pspec_justMP.py $DIR 15 21
+
+
+
+
+
+
+#
